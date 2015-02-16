24d1dbd2a19151408c9f485448323f88de641d03
Sébastien Lucas <sebastien@slucas.fr>
2015-02-16 21:26:50 +0100

    First try with gulp -> create a single js / css for eacg bower deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
+'use strict';
+
 var gulp = require('gulp');
 
 // Import dependencies
 var jshint = require('gulp-jshint');
+var mainBowerFiles = require('main-bower-files');
+var concat = require('gulp-concat');
+var gulpFilter = require('gulp-filter');
+var rename = require('gulp-rename');
 
 var source = 'app/';
+
+var publishdir = 'public';
+var dist = {
+all: [publishdir + '/**/*'],
+css: publishdir + '/css/',
+js: publishdir + '/js/',
+font: publishdir + '/fonts/',
+vendor: publishdir + '/static/'
+};
 // Define tasks
 
 // Lint Task
@@ -11,4 +26,21 @@ gulp.task('lint', function () {
     gulp.src(source + 'js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bower', function() {
+    var jsFilter = gulpFilter('**/*.js');
+    var cssFilter = gulpFilter('**/*.css');
+    var fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
+    return gulp.src(mainBowerFiles())
+        .pipe(jsFilter)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(dist.js))
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest(dist.css))
+        .pipe(cssFilter.restore())
+        .pipe(fontFilter)
+        .pipe(gulp.dest(dist.font));
 });
