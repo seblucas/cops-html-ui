c1d5839e011480da7a3342f30297042916f908ce
Sébastien Lucas <sebastien@slucas.fr>
2015-02-18 20:53:17 +0100

    Working first test

--- app/category/category-list.controller.spec.js
+++ app/category/category-list.controller.spec.js
@@ -2,22 +2,45 @@
 
 describe('categoryListController', function(){
 
-  beforeEach(module('Cops.category'));
+  beforeEach(module('restangular'));
+  beforeEach(module('Cops.category'), function(RestangularProvider) {
+    RestangularProvider.setBaseUrl('http://phoenix');
+  });
+
   //beforeEach(angular.mock.module('Cops.category'));
 
-  var $controller;
+  var scope, ListCtrl, httpBackend, stateParams, Restangular;
 
-  beforeEach(inject(function(_$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-  }));
+  beforeEach(inject(function ($controller, _$httpBackend_, $rootScope, _Restangular_) {
+        var controllerHelperServices = {
+          initController: function() {}
+        };
+        var spinnerService = {
+          hide: function() {},
+          show: function() {}
+        };
+        httpBackend = _$httpBackend_;
+        httpBackend.expectGET('/databases/0/authors').respond('[{"name":"tester"},{"name":"tester2"}]');
+        Restangular = _Restangular_;
+        stateParams = {db: 0, cat : 'authors'};
+        scope = $rootScope.$new();
+        ListCtrl = $controller('categoryListController', {
+            $scope: scope,
+            $stateParams: stateParams,
+            Restangular: Restangular,
+            controllerHelperServices: controllerHelperServices,
+            spinnerService: spinnerService
+          });
+      }));
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
 
 
   it('should create "phones" model with 3 phones', function() {
-    var $scope = {};
-    var controller = $controller('categoryListController', { $scope: $scope });
-
-    expect($scope.currentTemplate).toBe('partials/category.list.html');
+    httpBackend.flush();
+    expect(scope.currentTemplate).toBe('partials/category.list.html');
   });
 
 });
