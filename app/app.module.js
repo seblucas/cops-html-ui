ac01db72d72dee6079e8497bd7ddc80c1d81fd27
Sébastien Lucas <sebastien@slucas.fr>
2015-02-22 10:14:56 +0100

    Create a module for routes

--- app/app.module.js
+++ app/app.module.js
@@ -3,7 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('Cops', [
-  'ui.router',
+  'Cops.routes',
   'ngAnimate',
   'Cops.controllers',
   'Cops.category',
@@ -19,12 +19,4 @@ angular.module('Cops', [
   'seblucas.slSpinner',
   'seblucas.slStaticInclude',
   'restangular'
-]).
-run([ '$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
-  // It's very handy to add references to $state and $stateParams to the $rootScope
-  // so that you can access them from any scope within your applications.For example,
-  // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-  // to active whenever 'contacts.list' or one of its decendents is active.
-  $rootScope.$state = $state;
-  $rootScope.$stateParams = $stateParams;
-}]);
+]);
--- app/app.route.js
+++ app/app.route.js
@@ -1,9 +1,15 @@
 'use strict';
 
 
-var app = angular.module('Cops');
-
-app.
+angular.module('Cops.routes', ['ui.router']).
+run([ '$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+  // It's very handy to add references to $state and $stateParams to the $rootScope
+  // so that you can access them from any scope within your applications.For example,
+  // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+  // to active whenever 'contacts.list' or one of its decendents is active.
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}]).
 config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /
   $urlRouterProvider.otherwise('/');
