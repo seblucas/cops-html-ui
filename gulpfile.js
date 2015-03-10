536c262c5b6b81ba861a5994326c6bfb0766097e
Sébastien Lucas <sebastien@slucas.fr>
2015-03-10 17:14:37 +0100

    The build should now fail (some linting error remaining)

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,8 @@ gulp.task('bootlint', function () {
 gulp.task('lint', function () {
     gulp.src(jsSources)
         .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('bower', function() {
