48227c68d556551a4c363d8f673a10ba1f7e16dd
Sébastien Lucas <sebastien@slucas.fr>
2015-03-17 15:02:50 +0100

    Add a unit test for the configuration (based on localforage tests)

---
+++ app/services/configuration.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+describe('configurationServices', function(){
+
+  var instanceVersion = 0, configurationServices, rootScope, $localForage;
+
+  var triggerDigests = function() {
+    return setInterval(function() {
+      rootScope.$digest();
+    }, 10);
+  };
+  var stopDigests = function(interval) {
+    window.clearInterval(interval);
+  };
+
+  beforeEach(function() {
+    module('Cops.services');
+    inject(function (_configurationServices_, _$rootScope_, _$localForage_) {
+      rootScope = _$rootScope_;
+      $localForage = _$localForage_;
+      configurationServices = _configurationServices_;
+    });
+  });
+
+  afterEach(function(done) {
+    var interval = triggerDigests();
+    // create a fresh instance
+    $localForage.clear().then(function() {
+      $localForage = $localForage.createInstance({
+        name: ++instanceVersion
+      });
+      stopDigests(interval);
+      done();
+    }, done);
+  });
+
+  it('should use the webSQL storage driver', function() {
+    expect(configurationServices.getDriver()).toBe('webSQLStorage');
+  });
+
+  it('should return the configuration default values', function(done) {
+    var interval = triggerDigests();
+    configurationServices.load().then(function(conf) {
+      stopDigests(interval);
+      expect(conf.booksPerPage).toBe(96);
+      done();
+    });
+  });
+});
