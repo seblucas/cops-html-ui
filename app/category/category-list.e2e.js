1120de7731aa313073259aa5a5aa58668b391379
Sébastien Lucas <sebastien@slucas.fr>
2015-04-17 16:42:52 +0200

    Reformat the test to make them smaller and add a wait to reduce the false positive

--- app/category/category-list.e2e.js
+++ app/category/category-list.e2e.js
@@ -2,9 +2,16 @@
 
 describe('Category list', function() {
 
+  var nextButton;
+
   beforeEach(function() {
     browser.get('index_prod.html#/0/authors');
-    browser.waitForAngular();
+    nextButton = element(by.css('[ng-click="selectPage(page + 1)"]'));
+
+    // We'll have to wait for at least an item or it may give false positive
+    browser.wait(function(){
+      return element(by.css('.list-group-item')).isPresent();
+    }, 1000);
   });
 
   it('should have the first page starting by a A', function() {
@@ -16,9 +23,7 @@ describe('Category list', function() {
   });
 
   it('should have the second page starting by anything but a A', function() {
-    var nextButton = element(by.css('[ng-click="selectPage(page + 1)"]'));
     nextButton.click();
-    browser.waitForAngular();
     expect(element.all(by.css('.list-group-item')).first().getText()).not.toMatch(/^A/);
   });
 });
