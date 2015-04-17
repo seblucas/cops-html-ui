9afc51d184652075552ede9a5a63886fc3e5690b
Sébastien Lucas <sebastien@slucas.fr>
2015-04-17 16:42:17 +0200

    Disable animation for the protractor tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,17 @@
 'use strict';
 
 exports.config = {
+  onPrepare: function() {
+
+    // Disable animations so e2e tests run more quickly
+    var disableNgAnimate = function() {
+      angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
+        $animate.enabled(false);
+      }]);
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+  },
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost/ncops-angular/public/',
   capabilities: {
