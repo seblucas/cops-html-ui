4875c7b63f3b858a2400b10f3573a71201057716
Sébastien Lucas <sebastien@slucas.fr>
2015-04-20 17:55:03 +0200

    Remove unneeded method

--- app/services/controller-helper.service.js
+++ app/services/controller-helper.service.js
@@ -4,14 +4,6 @@ var app = angular.module('Cops.services');
 
 app.factory('controllerHelperServices', ['$q', 'configurationServices', function($q, configurationServices) {
   return {
-    initController: function (scope, withPaging) {
-      if (withPaging) {
-        scope.itemsPerPage = 48;
-        scope.itemsPerPageList = configurationServices.getPageSizes();
-        scope.maxSize = 10;
-        scope.currentPage = 1;
-      }
-    },
     initControllerWithPaging: function (withBooks) {
       var deferred = $q.defer();
       var paging = {
