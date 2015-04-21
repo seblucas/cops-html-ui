60aa8264a6ca4da0eff47911093b08a7b77e965c
Sébastien Lucas <sebastien@slucas.fr>
2015-04-21 08:03:33 +0200

    test: Missing method initControllerWithPaging in test

--- app/services/controller-helper.spec.js
+++ app/services/controller-helper.spec.js
@@ -2,12 +2,12 @@
 
 describe('controllerHelperServices', function(){
 
-  var configurationServices, controllerHelperServices, rootScope;
+  var configurationServices, controllerHelperServices, $rootScope;
 
   beforeEach(function() {
     module('Cops.services');
     inject(function (_configurationServices_, _controllerHelperServices_, _$rootScope_) {
-      rootScope = _$rootScope_;
+      $rootScope = _$rootScope_;
       controllerHelperServices = _controllerHelperServices_;
       configurationServices = _configurationServices_;
     });
@@ -44,4 +44,52 @@ describe('controllerHelperServices', function(){
       expect(configurationServices.setValue.calls.allArgs()).toEqual([['categoriesGridListTemplate', 'list']]);
     });
   });
+
+  describe('initControllerWithPaging', function() {
+    var triggerDigests = function() {
+      return setInterval(function() {
+        $rootScope.$digest();
+      }, 5);
+    };
+    var stopDigests = function(interval) {
+      window.clearInterval(interval);
+    };
+
+    it('should set itemsPerPage using booksPerPage', function(done) {
+      var interval = triggerDigests();
+      controllerHelperServices.initControllerWithPaging(true).then(function(paging) {
+        stopDigests(interval);
+        expect(paging.itemsPerPage).toBe(96);
+        done();
+      });
+    });
+
+    it('should set currentTemplate using booksGridListTemplate', function(done) {
+      var interval = triggerDigests();
+      controllerHelperServices.initControllerWithPaging(true).then(function(paging) {
+        stopDigests(interval);
+        expect(paging.currentTemplate).toBe('th');
+        done();
+      });
+    });
+
+    it('should set itemsPerPage using categoriesPerPage', function(done) {
+      var interval = triggerDigests();
+      controllerHelperServices.initControllerWithPaging(false).then(function(paging) {
+        stopDigests(interval);
+        expect(paging.itemsPerPage).toBe(192);
+        done();
+      });
+    });
+
+    it('should set currentTemplate using categoriesGridListTemplate', function(done) {
+      var interval = triggerDigests();
+      controllerHelperServices.initControllerWithPaging(false).then(function(paging) {
+        stopDigests(interval);
+        expect(paging.currentTemplate).toBe('list');
+        done();
+      });
+    });
+  });
+
 });
