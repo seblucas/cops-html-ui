81520efd65578449649978817322eb1a879d50cb
Sébastien Lucas <sebastien@slucas.fr>
2016-01-30 17:08:36 +0100

    Change the way we wait for the browser to be ready (the previous way was not working with latest protractor update).
    Use ExpectedConditions now.

--- app/category/category-list.e2e.js
+++ app/category/category-list.e2e.js
@@ -2,29 +2,23 @@
 
 describe('Category list', function() {
 
-  var nextButton, items, itemsPerPage, toggleBtn;
+  var nextButton, items, itemsTag, itemsPerPage, toggleBtn;
   var baseUrl = '#/0/authors';
+  var EC = protractor.ExpectedConditions;
 
   var waitForPage = function() {
-    // We'll have to wait for at least an item or it may give false positive
-    // Don't really know why
-    //  * Maybe because of the static include
-    //  * Maybe because of the some other directive
-    browser.wait(function(){
-      return element(by.css('.list-group-item')).isPresent() &&
-             element(by.css('ul.pagination > li.active')).isPresent() &&
-             element(by.repeater('item in toggles')).isPresent();
-    }, 1000);
+    browser.wait(EC.presenceOf(itemsTag), 10000, 'Main list not found');
   };
 
   beforeEach(function() {
     browser.get(baseUrl);
+    //waitForPage();
+    browser.waitForAngular();
     nextButton = element(by.css('[ng-click="selectPage(page + 1, $event)"]'));
     items = element.all(by.repeater('item in list'));
+    itemsTag = element(by.repeater('item in list'));
     itemsPerPage = element.all(by.repeater('itemValue in itemsPerPageList'));
     toggleBtn = element.all(by.repeater('item in toggles'));
-
-    waitForPage();
   });
 
   afterEach(function () {
@@ -33,10 +27,12 @@ describe('Category list', function() {
 
   describe('paging', function() {
     it('should have the first page starting by a A', function() {
+      waitForPage();
       expect(items.first().getText()).toMatch(/^A/);
     });
 
     it('should have the second page starting by anything but a A', function() {
+      waitForPage();
       nextButton.click();
       expect(items.first().getText()).not.toMatch(/^A/);
     });
@@ -45,19 +41,22 @@ describe('Category list', function() {
 
   describe('items per page', function() {
     it('should have 192 items (default value)', function() {
+      waitForPage();
       expect(items.count()).toBe(192);
     });
 
     it('should have 24 items if the 24 button is choosen', function() {
+      waitForPage();
       itemsPerPage.first().element(by.linkText('24')).click();
       expect(items.count()).toBe(24);
     });
 
     it('should save the prefered number of items per page', function() {
+      waitForPage();
       itemsPerPage.first().element(by.linkText('24')).click();
       browser.waitForAngular();
-      browser.sleep(100);
-      browser.get(baseUrl);
+      browser.sleep(200);
+      browser.refresh();
       waitForPage();
       expect(items.count()).toBe(24);
     });
@@ -66,15 +65,17 @@ describe('Category list', function() {
 
   describe('grid / list toggle', function() {
     it('should switch to card view if it is clicked', function() {
+      waitForPage();
       toggleBtn.first().click();
       expect(element(by.css('.panel')).isPresent()).toBeTruthy();
     });
 
     it('should save the prefered view', function() {
+      waitForPage();
       toggleBtn.first().click();
       browser.waitForAngular();
-      browser.sleep(100);
-      browser.get(baseUrl);
+      browser.sleep(200);
+      browser.refresh();
       waitForPage();
       expect(element(by.css('.panel')).isPresent()).toBeTruthy();
     });
