133c60dc916dd08e9a4833ade59736f47f10b736
Sébastien Lucas <sebastien@slucas.fr>
2016-02-06 16:56:16 +0100

    test: let's retry with chrome

--- protractor.conf.js
+++ protractor.conf.js
@@ -24,14 +24,15 @@ if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.multiCapabilities = [
-  /*{
+  {
     'name': 'cops-html-ui chrome Linux node v' + process.env.TRAVIS_NODE_VERSION,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'browserName': 'chrome',
     'platform': 'Linux',
+    'seleniumVersion': '2.48.2',
     'build': process.env.TRAVIS_BUILD_NUMBER
   },
-  {
+  /*{
     'name': 'cops-html-ui chrome W10 node v' + process.env.TRAVIS_NODE_VERSION,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'browserName': 'chrome',
@@ -44,19 +45,19 @@ if (process.env.TRAVIS) {
     'browserName': 'microsoftedge',
     'platform': 'Windows 10',
     'build': process.env.TRAVIS_BUILD_NUMBER
-  },*/
-  {
-    'name': 'cops-html-ui firefox node v' + process.env.TRAVIS_NODE_VERSION,
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'browserName': 'firefox',
-    'seleniumVersion': '2.48.2',
-    'build': process.env.TRAVIS_BUILD_NUMBER
   },
   {
     'name': 'cops-html-ui safari node v' + process.env.TRAVIS_NODE_VERSION,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'browserName': 'safari',
     'platform': 'OS X 10.11',
+    'seleniumVersion': '2.48.2',
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  },*/
+  {
+    'name': 'cops-html-ui firefox node v' + process.env.TRAVIS_NODE_VERSION,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'browserName': 'firefox',
     'seleniumVersion': '2.48.2',
     'build': process.env.TRAVIS_BUILD_NUMBER
   }];
