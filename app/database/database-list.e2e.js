1422ad312727f76b9cb8106a05356b1305b15dd2
Sébastien Lucas <sebastien@slucas.fr>
2016-02-06 16:00:24 +0100

    test: Database seems to be failing with sauce, so add a wait (same as category)

--- app/database/database-list.e2e.js
+++ app/database/database-list.e2e.js
@@ -1,20 +1,33 @@
 'use strict';
 
 describe('Database list', function() {
+  var items, itemsTag;
+  var EC = protractor.ExpectedConditions;
+
+  var waitForPage = function() {
+    browser.wait(EC.presenceOf(itemsTag), 10000, 'Main list not found');
+  };
 
   beforeEach(function() {
     browser.get('#/');
+
+    browser.waitForAngular();
+    items = element.all(by.repeater('database in databases'));
+    itemsTag = element(by.repeater('database in databases'));
   });
 
   it('should have a H1 title', function() {
+    waitForPage();
     expect(element(by.tagName('h1')).getText()).toEqual('All');
   });
 
   it('should have 2 panels', function() {
+    waitForPage();
     expect(element.all(by.css('.panel')).count()).toEqual(2);
   });
 
   it('should have 2 panels (repeater)', function() {
-    expect(element.all(by.repeater('database in databases')).count()).toEqual(2);
+    waitForPage();
+    expect(items.count()).toEqual(2);
   });
 });
