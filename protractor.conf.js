5ec34980f63b043f9129d72a10952cf54e79659f
Sébastien Lucas <sebastien@slucas.fr>
2016-02-06 16:29:17 +0100

    test: Fix the version of safari, add chrome with windows and Edge

--- protractor.conf.js
+++ protractor.conf.js
@@ -24,9 +24,24 @@ if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.multiCapabilities = [{
-    'name': 'cops-html-ui chrome node v' + process.env.TRAVIS_NODE_VERSION,
+    'name': 'cops-html-ui chrome Linux node v' + process.env.TRAVIS_NODE_VERSION,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'browserName': 'chrome',
+    'platform': 'Linux',
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  },
+  {
+    'name': 'cops-html-ui chrome W10 node v' + process.env.TRAVIS_NODE_VERSION,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'browserName': 'chrome',
+    'platform': 'Windows 10',
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  },
+  {
+    'name': 'cops-html-ui Edge node v' + process.env.TRAVIS_NODE_VERSION,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'browserName': 'microsoftedge',
+    'platform': 'Windows 10',
     'build': process.env.TRAVIS_BUILD_NUMBER
   },
   {
@@ -39,6 +54,7 @@ if (process.env.TRAVIS) {
     'name': 'cops-html-ui safari node v' + process.env.TRAVIS_NODE_VERSION,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'browserName': 'safari',
+    'platform': 'OS X 10.11',
     'build': process.env.TRAVIS_BUILD_NUMBER
   }];
 }
