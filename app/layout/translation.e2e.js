eb6cee688be2635d6a74d45b6b61d357a9110d28
Sébastien Lucas <sebastien@slucas.fr>
2016-02-11 22:05:31 +0100

    test: Change the way the test are made to have the string shown in case of error. Add a test for the language storage

--- app/layout/translation.e2e.js
+++ app/layout/translation.e2e.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Translation', function() {
-  var items, itemsTag, languageMenu, languageMenuToggle, languageMenuList;
+  var items, itemsTag, languageMenu, languageMenuToggle, languageMenuList, authorLink;
   var EC = protractor.ExpectedConditions;
 
   var waitForPage = function() {
@@ -17,19 +17,32 @@ describe('Translation', function() {
     languageMenu = element(by.id('languageMenu'));
     languageMenuToggle = languageMenu.element(by.className('dropdown-toggle'));
     languageMenuList = languageMenu.element(by.className('dropdown-menu'));
+    authorLink = items.first().element(by.css('a[ui-sref="base.category.list({cat: category.name})"]'));
   });
 
-  it('should have an english author string if English is selected', function() {
-    waitForPage();
-    languageMenuToggle.click(); // open the menu
-    languageMenuList.element(by.linkText('English')).click();
-    expect(items.first().element(by.linkText('Authors')).isPresent()).toBe(true);
-  });
+  describe('of the authors.title placeholder', function() {
+    it('should have an english author string by default', function() {
+      waitForPage();
+      expect(authorLink.getText()).toEqual('Authors');
+    });
 
-  it('should have an french author string if French is selected', function() {
-    waitForPage();
-    languageMenuToggle.click(); // open the menu
-    languageMenuList.element(by.linkText('Français')).click();
-    expect(items.first().element(by.linkText('Auteurs')).isPresent()).toBe(true);
+    it('should have an french author string if French is selected', function() {
+      waitForPage();
+      languageMenuToggle.click(); // open the menu
+      languageMenuList.element(by.linkText('Français')).click();
+      expect(authorLink.getText()).toEqual('Auteurs');
+    });
+
+    xit('should have an french author string even after a refresh', function() {
+      waitForPage();
+      languageMenuToggle.click(); // open the menu
+      languageMenuList.element(by.linkText('Français')).click();
+      browser.sleep(200);
+      browser.refresh();
+      waitForPage();
+      browser.pause();
+      expect(items.first().element(by.linkText('Auteurs')).isPresent()).toBe(true);
+    }).pend('The language is not saved in the localStorage for now');
+
   });
 });
